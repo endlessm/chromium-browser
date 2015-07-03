19d6c5fbff46a27730a32dc3f0953338c4d0a0de
Daniel Drake <drake@endlessm.com>
2015-07-03 14:59:40 -0600

    Import endless-Search-for-extra-codecs-under-var-directory.patch from Ubuntu packaging

--- media/base/media_posix.cc
+++ media/base/media_posix.cc
@@ -9,6 +9,7 @@
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/strings/stringize_macros.h"
+#include "chrome/common/chrome_version_info_values.h"
 #include "media/ffmpeg/ffmpeg_common.h"
 #include "third_party/ffmpeg/ffmpeg_stubs.h"
 
@@ -49,7 +50,23 @@ bool InitializeMediaLibraryInternal(const base::FilePath& module_dir) {
 
   // First try to initialize with Chrome's sumo library.
   DCHECK_EQ(kNumStubModules, 1);
+
+#if defined(OS_POSIX)
+  // For Endless OS, we check the location for extra codecs first of all,
+  // which will follow a naming scheme including Chrome's current version,
+  // in order to facilitate upgrades of the browsers and these extra codecs.
+  base::FilePath extra_codecs_dir(FILE_PATH_LITERAL("/var/lib/codecs/chromium-browser"));
+  base::FilePath extra_codecs_path = extra_codecs_dir.Append(
+      FILE_PATH_LITERAL(DSO_NAME("ffmpegsumo", PRODUCT_VERSION)));
+
+  // The expected filename will be something like 'libffmpegsumo.so.40.0.2214.111'.
+  if (base::PathExists(extra_codecs_path))
+      paths[kModuleFfmpegsumo].push_back(extra_codecs_path.value());
+  else
+      paths[kModuleFfmpegsumo].push_back(module_dir.Append(kSumoLib).value());
+#else
   paths[kModuleFfmpegsumo].push_back(module_dir.Append(kSumoLib).value());
+#endif
 
   // If that fails, see if any system libraries are available.
   paths[kModuleFfmpegsumo].push_back(module_dir.Append(
