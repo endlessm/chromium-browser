655afa365e643168eb267c935d3776c96fbc3bd3
Daniel Drake <drake@endlessm.com>
2015-07-03 14:58:53 -0600

    Import endless-external-protocol-help.patch from Ubuntu packaging

--- chrome/browser/external_protocol/external_protocol_handler.cc
+++ chrome/browser/external_protocol/external_protocol_handler.cc
@@ -180,6 +180,7 @@ void ExternalProtocolHandler::PrepopulateDictionary(
 
   static const char* const allowed_schemes[] = {
     "endlessm-app",
+    "help",
     "mailto",
     "news",
     "snews",
