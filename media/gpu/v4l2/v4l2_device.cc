8e9c092f077f00135ebaa183239c2cd92c4e639e
Daniel Drake <drake@endlessm.com>
2015-07-03 14:58:49 -0600

    Enable V4L2 accelerated video decoding on Linux/ARM
    Some of the flags that were being changed at 38b6ff1babe
    are now being handled by USE_VAAPI buildflag. We probably
    need to check how this is being configured for our
    archs.

--- media/gpu/v4l2/v4l2_device.cc
+++ media/gpu/v4l2/v4l2_device.cc
@@ -823,7 +823,7 @@ scoped_refptr<V4L2Device> V4L2Device::Create() {
 
   scoped_refptr<V4L2Device> device;
 
-#if defined(ARCH_CPU_ARMEL)
+#if defined(ARCH_CPU_ARMEL) && defined(OS_CHROMEOS)
   device = new TegraV4L2Device();
   if (device->Initialize())
     return device;
@@ -843,9 +843,6 @@ VideoPixelFormat V4L2Device::V4L2PixFmtToVideoPixelFormat(uint32_t pix_fmt) {
     case V4L2_PIX_FMT_NV12:
     case V4L2_PIX_FMT_NV12M:
       return PIXEL_FORMAT_NV12;
-
-    case V4L2_PIX_FMT_MT21C:
-      return PIXEL_FORMAT_MT21;
 
     case V4L2_PIX_FMT_YUV420:
     case V4L2_PIX_FMT_YUV420M:
@@ -873,9 +870,6 @@ uint32_t V4L2Device::VideoPixelFormatToV4L2PixFmt(const VideoPixelFormat format,
   switch (format) {
     case PIXEL_FORMAT_NV12:
       return single_planar ? V4L2_PIX_FMT_NV12 : V4L2_PIX_FMT_NV12M;
-    case PIXEL_FORMAT_MT21:
-      // No single plane format for MT21.
-      return single_planar ? 0 : V4L2_PIX_FMT_MT21C;
     case PIXEL_FORMAT_I420:
       return single_planar ? V4L2_PIX_FMT_YUV420 : V4L2_PIX_FMT_YUV420M;
     case PIXEL_FORMAT_YV12:
