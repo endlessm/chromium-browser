166300a65f324b69a541c11294169ba174f2164f
Daniel Drake <drake@endlessm.com>
2015-09-28 17:05:51 -0600

    Don't use virtualized GL Contexts on Linux/ARM
    https://codereview.chromium.org/1081533003 switches all Linux to
    virtualized GL contexts, apparently that improves performance,
    but it seems quite Intel-centric.
    
    Virtualized contexts are broken on EGL. We end up in
    GLContextVirtual::Initialize with a compatible_surface which is an
    instance of NativeViewGLSurfaceEGL; an EGL surface correctly based on the
    X11 window with appropriate pixel depth (no alpha).
    
    Then in GLContextEGL::MakeCurrent we use the local EGLContext (context_),
    which was created from with a EGLConfig from
    PbufferGLSurfaceEGL::GetConfig which was specifically selected in
    GLSurfaceEGL::InitializeOneOff to have an alpha channel.
    
    eglMakeCurrent then returns EGL_BAD_MATCH due to the alpha depth mismatch.
    
    Avoid virtualized GL contexts on ARM for the time being.
    I left it active on Intel assuming that there is a performance benefit.

--- gpu/config/gpu_driver_bug_list_json.cc
+++ gpu/config/gpu_driver_bug_list_json.cc
@@ -1241,6 +1241,7 @@ LONG_STRING_CONST(
         "disable_gl_rgb_format"
       ]
     },
+#ifndef __arm__
     {
       "id": 109,
       "description": "MakeCurrent is slow on Linux with NVIDIA drivers",
@@ -1252,6 +1253,7 @@ LONG_STRING_CONST(
         "use_virtualized_gl_contexts"
       ]
     },
+#endif
     {
       "id": 110,
       "description": "EGL Sync server causes crashes on Adreno 2xx and 3xx drivers",
