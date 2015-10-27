95c3f5d4d608976bdbff7591ea7c8aec6cef4cf6
Mario Sanchez Prada <mario@endlessm.com>
2015-10-27 09:56:28 +0000

    Don't ever play the default "First Run experience"
    We don't want to ever show the bubble overlay popping out of the address
    bar on the first run, nor the Getting Started webpage on a second tab.
    
    This change has the problem that it invalidates the master_preferences
    feature, as those get imported only during the first run, but it should
    be fine as we are not using that now (nor planning to use it in the near
    future) and that is a feature already on its way to being deprecated.
    
    [endlessm/eos-shell#5937]

--- chrome/browser/first_run/first_run.cc
+++ chrome/browser/first_run/first_run.cc
@@ -74,7 +74,6 @@ namespace {
 uint16_t g_auto_import_state = first_run::AUTO_IMPORT_NONE;
 
 // Flags for functions of similar name.
-bool g_should_show_welcome_page = false;
 bool g_should_do_autofill_personal_data_manager_first_run = false;
 
 // Indicates whether this is first run. Populated when IsChromeFirstRun
@@ -352,15 +351,10 @@ void RegisterProfilePrefs(
 }
 
 bool IsChromeFirstRun() {
-  if (g_first_run == internal::FIRST_RUN_UNKNOWN) {
-    const base::CommandLine* command_line =
-        base::CommandLine::ForCurrentProcess();
-    g_first_run = internal::DetermineFirstRunState(
-        internal::IsFirstRunSentinelPresent(),
-        command_line->HasSwitch(switches::kForceFirstRun),
-        command_line->HasSwitch(switches::kNoFirstRun));
-  }
-  return g_first_run == internal::FIRST_RUN_TRUE;
+  // We don't ever want a "First Run experience" to happen, so we
+  // just create the sentinel if needed and always return false.
+  CreateSentinelIfNeeded();
+  return false;
 }
 
 #if defined(OS_MACOSX)
@@ -377,7 +371,7 @@ bool IsMetricsReportingOptIn() {
 }
 
 void CreateSentinelIfNeeded() {
-  if (IsChromeFirstRun())
+  if (!internal::IsFirstRunSentinelPresent())
     internal::CreateSentinel();
 
   // Causes the first run sentinel creation time to be read and cached, while
@@ -397,13 +391,10 @@ void ResetCachedSentinelDataForTesting() {
 }
 
 void SetShouldShowWelcomePage() {
-  g_should_show_welcome_page = true;
 }
 
 bool ShouldShowWelcomePage() {
-  bool retval = g_should_show_welcome_page;
-  g_should_show_welcome_page = false;
-  return retval;
+  return false;
 }
 
 bool IsOnWelcomePage(content::WebContents* contents) {
