1f53f0e8429db30a37555d8d4c35daf99ff189e7
Mario Sanchez Prada <mario@endlessm.com>
2015-10-27 09:56:28 +0000

    Don't ever play the default "First Run experience"
    We don't want to ever show the bubble overlay popping out of the address
    bar on the first run, nor the Getting Started webpage on a second tab.
    
    This change has the problem that it invalidates the master_preferences
    feature, as those get imported only during the first run, but it should
    be fine as we are not using that now (nor planning to use it in the near
    future) and that is a feature already on its way to being deprecated.
    
    [endlessm/eos-shell#5937]

--- chrome/browser/ui/startup/startup_tab_provider.cc
+++ chrome/browser/ui/startup/startup_tab_provider.cc
@@ -213,13 +213,6 @@ bool StartupTabProviderImpl::ShouldShowWelcomeForOnboarding(
 StartupTabs StartupTabProviderImpl::GetStandardOnboardingTabsForState(
     const StandardOnboardingTabsParams& params) {
   StartupTabs tabs;
-  if (CanShowWelcome(params.is_signin_allowed, params.is_supervised_user,
-                     params.is_force_signin_enabled) &&
-      ShouldShowWelcomeForOnboarding(params.has_seen_welcome_page,
-                                     params.is_signed_in,
-                                     params.is_signin_in_progress)) {
-    tabs.emplace_back(GetWelcomePageUrl(!params.is_first_run), false);
-  }
   return tabs;
 }
 
