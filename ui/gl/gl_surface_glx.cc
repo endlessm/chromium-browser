7f5c3383ad22543ffd33228eb41ec508267f203f
Mario Sanchez Prada <mario@endlessm.com>
2015-11-09 23:43:26 +0000

    desktop_aura: Use a white pixel to fill the XWindow's background
    Instead of using nothing, which causes some weird effects when launching
    chromium (e.g. XWindow initialized with data from the front buffer), while
    the first expose event is not attended, define instead a solid colour
    for the background so that at least the startup experience is consistent.
    
    The whet pixel has the side effect of making resizing quite disturbing,
    since the background is quite visible while resizing due to the slow
    rendering process. So make sure that the XWindow's background is unset
    (set to None) once the window has been Mapped, thus ensuring the
    resizing experience does not regress. Wait until the first SwapBuffers
    before resetting XWindow's background pixmap as a temporary workaround
    for part of the screen being filled in white and the rest black on
    startup, while we work on fixing this upstream Chromium bug #554008.
    
    [endlessm/eos-shell#5873]
    [endlessm/eos-shell#6068]

--- ui/gl/gl_surface_glx.cc
+++ ui/gl/gl_surface_glx.cc
@@ -521,7 +521,8 @@ NativeViewGLSurfaceGLX::NativeViewGLSurfaceGLX(gfx::AcceleratedWidget window)
       window_(0),
       glx_window_(0),
       config_(nullptr),
-      visual_id_(CopyFromParent) {}
+      visual_id_(CopyFromParent),
+      hasSwappedBuffers_(false) {}
 
 GLXDrawable NativeViewGLSurfaceGLX::GetDrawableHandle() const {
   return glx_window_;
@@ -608,6 +609,20 @@ gfx::SwapResult NativeViewGLSurfaceGLX::SwapBuffers() {
   TRACE_EVENT2("gpu", "NativeViewGLSurfaceGLX:RealSwapBuffers", "width",
                GetSize().width(), "height", GetSize().height());
   glXSwapBuffers(g_display, GetDrawableHandle());
+
+  // FIXME: We need to restore the background pixel that we set to WhitePixel
+  // on views::DesktopWindowTreeHostX11::InitX11Window back to None for the XWindow
+  // associated to this surface after the first SwapBuffers has happened, to avoid
+  // showing a weird white background while resizing.
+  //
+  // This is probably not the right way to do it, so we try to do it only once for the
+  // lifetime of the surface, while we keep working on the proper solution upstream.
+  // https://code.google.com/p/chromium/issues/detail?id=554008
+  if (!hasSwappedBuffers_) {
+    XSetWindowBackgroundPixmap(g_display, parent_window_, 0);
+    hasSwappedBuffers_ = true;
+  }
+
   return gfx::SwapResult::SWAP_ACK;
 }
 
