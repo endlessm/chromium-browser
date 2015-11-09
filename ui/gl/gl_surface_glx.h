991098adf6df86135dc3d2017da7750d40ce0c52
Mario Sanchez Prada <mario@endlessm.com>
2015-11-09 23:43:26 +0000

    desktop_aura: Use a white pixel to fill the XWindow's background
    Instead of using nothing, which causes some weird effects when launching
    chromium (e.g. XWindow initialized with data from the front buffer), while
    the first expose event is not attended, define instead a solid colour
    for the background so that at least the startup experience is consistent.
    
    The whet pixel has the side effect of making resizing quite disturbing,
    since the background is quite visible while resizing due to the slow
    rendering process. So make sure that the XWindow's background is unset
    (set to None) once the window has been Mapped, thus ensuring the
    resizing experience does not regress. Wait until the first SwapBuffers
    before resetting XWindow's background pixmap as a temporary workaround
    for part of the screen being filled in white and the rest black on
    startup, while we work on fixing this upstream Chromium bug #554008.
    
    [endlessm/eos-shell#5873]
    [endlessm/eos-shell#6068]

--- ui/gl/gl_surface_glx.h
+++ ui/gl/gl_surface_glx.h
@@ -112,6 +112,10 @@ class GL_EXPORT NativeViewGLSurfaceGLX : public GLSurfaceGLX {
 
   std::unique_ptr<gfx::VSyncProvider> vsync_provider_;
 
+  // FIXME: Part of temporary hack to restore the background pixel for the
+  // underlying XWindow. See NativeViewGLSurfaceGLX::SwapBuffers for details.
+  bool hasSwappedBuffers_;
+
   DISALLOW_COPY_AND_ASSIGN(NativeViewGLSurfaceGLX);
 };
 
