288314e120b7a97d0e1c1df5055afb165c17c9ef
Mario Sanchez Prada <mario@endlessm.com>
2015-12-22 14:42:38 +0000

    Always use buttons from chromium's classic theme, even for GTK+ theme
    The navigation buttons from the GTK+ theme are rendered very blurry and
    jagged when running with higher text-scaling-factor values (higher DPI)
    and, on top of that, they look worse in general than the ones from the
    classic theme, so make sure that we these other ones instead.
    
    Note that other definitions from the GTK+ Theme, as defined via the relevant
    gtkrc file provided for the Adwaita theme, will be still used by chromium
    to render other details of the UI. This change affects the buttons only.
    
    [endlessm/eos-shell#5869]

--- chrome/browser/themes/theme_service.cc
+++ chrome/browser/themes/theme_service.cc
@@ -24,6 +24,7 @@
 #include "chrome/browser/themes/theme_syncable_service.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/pref_names.h"
+#include "components/grit/components_scaled_resources.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/user_metrics.h"
 #include "extensions/browser/extension_prefs.h"
@@ -103,6 +104,36 @@ bool IsColorGrayscale(SkColor color) {
   int b = SkColorGetB(color);
   int range = std::max(r, std::max(g, b)) - std::min(r, std::min(g, b));
   return range < kChannelTolerance;
+}
+
+bool IsThemeableImage(int id) {
+  // We don't want to theme some icons when using the GTK+ theme, since
+  // they look worse than the default ones, and quite bad when scaled up.
+  switch (id) {
+  case IDR_OMNIBOX_CALCULATOR:
+  case IDR_OMNIBOX_EXTENSION_APP:
+  case IDR_OMNIBOX_HTTP:
+  case IDR_OMNIBOX_SEARCH:
+  case IDR_OMNIBOX_STAR:
+  case IDR_OMNIBOX_TTS:
+  case IDR_BACK:
+  case IDR_BACK_D:
+  case IDR_FORWARD:
+  case IDR_FORWARD_D:
+  case IDR_HOME:
+  case IDR_RELOAD:
+  case IDR_RELOAD_D:
+  case IDR_STOP:
+  case IDR_STOP_D:
+    return false;
+
+  default:
+    // See Gtk2UI::GenerateGtkThemeBitmap() in gtk2_ui.cc to get
+    // an idea of which types of images would be in this case.
+    return true;
+  }
+
+  return true;
 }
 
 }  // namespace
@@ -187,9 +218,8 @@ void ThemeService::Init(Profile* profile) {
 
 gfx::Image ThemeService::GetImageNamed(int id) const {
   DCHECK(CalledOnValidThread());
-
   gfx::Image image;
-  if (theme_supplier_.get())
+  if (IsThemeableImage(id) && theme_supplier_.get())
     image = theme_supplier_->GetImageNamed(id);
 
   if (image.IsEmpty())
