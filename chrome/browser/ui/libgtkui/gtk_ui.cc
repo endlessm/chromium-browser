abe9f7bae9c090a13de45ff9daf69bf4b0143fd6
Mario Sanchez Prada <mario@endlessm.com>
2016-01-08 15:31:13 +0000

    Consider com.endlessm.CompositeMode.text-scaling-factor GSetting if available
    If the eos-composite-mode package is installed, the
    com.endlessm.CompositeMode will be available to provide an additional,
    accumulative, scaling factor to be used on top of the one from
    org.gnome.desktop.interface, so we need to check it here to determine the
    right value to set for the global Xft DPI setting.
    
    Contrary to what gnome-settings-daemon does (check the
    'text-scaling-factor' key from the schema), chromium checks the
    'browser-scaling-factor' key instead, which is defined by
    eos-composite-mode to be specifically used by the browser only, so that we
    can define an additional scaling factor just for it.
    
    [endlessm/eos-shell#6197]

--- chrome/browser/ui/libgtkui/gtk_ui.cc
+++ chrome/browser/ui/libgtkui/gtk_ui.cc
@@ -6,6 +6,7 @@
 
 #include <dlfcn.h>
 #include <math.h>
+#include <gio/gio.h>
 #include <pango/pango.h>
 
 #include <cmath>
@@ -457,8 +458,13 @@ GtkUi::GtkUi() {
   gtk_widget_realize(fake_window_);
 }
 
+// GSettings schema and key for the additional scaling factor defined by Endless.
+const char* kEndlessCompositeModeSchema = "com.endlessm.CompositeMode";
+const char* kEndlessCompositeModeScalingFactorKey = "browser-scaling-factor";
+
 GtkUi::~GtkUi() {
   gtk_widget_destroy(fake_window_);
+  g_object_unref(endless_gsettings_);
 }
 
 void OnThemeChanged(GObject* obj, GParamSpec* param, GtkUi* gtkui) {
@@ -496,6 +502,16 @@ void GtkUi::Initialize() {
   settings_provider_ = CreateSettingsProvider(this);
 
   indicators_count = 0;
+
+  /* The com.endlessm.CompositeMode schema will be present in some configurations only,
+   * so we need to do some extra checks here before assuming it's there. */
+  GSettingsSchema* schema = g_settings_schema_source_lookup(g_settings_schema_source_get_default(), kEndlessCompositeModeSchema, FALSE);
+  if (schema != NULL) {
+    endless_gsettings_ = g_settings_new_full(schema, NULL, NULL);
+    DVLOG(1) << "Found " << kEndlessCompositeModeSchema << " GSettings schema.";
+  } else {
+    DVLOG(1) << "No " << kEndlessCompositeModeSchema << " GSettings schema found.";
+  }
 
   // Instantiate the singleton instance of Gtk2EventLoop.
   Gtk2EventLoop::GetInstance();
@@ -1104,6 +1120,30 @@ bool GtkUi::GetChromeStyleColor(const char* style_property,
   return false;
 }
 
+float GtkUi::GetEndlessScalingFactor() const {
+  static float cached_result = -1.0f;
+
+  // The GSettings instance will be null in those configurations where
+  // we are not using eos-composite-mode, so we need to check this first;
+  if (!endless_gsettings_) {
+    return 1.0f;
+  }
+
+  // We only check the scaling factor on start up, not to confuse the UI in
+  // case the value of the GSettings key changes while chromium is running.
+  if (cached_result < 0.0f) {
+    double browser_scaling_factor = g_settings_get_double(endless_gsettings_, kEndlessCompositeModeScalingFactorKey);
+    DVLOG(1) << "GSetting browser-scaling-factor key found: " << browser_scaling_factor;
+
+    // The browse-scaling-factor should already be in [1.0, 2.0], but it
+    // does we'd better do some extra validation here, before applying it.
+    cached_result = std::max(1.0, std::min(browser_scaling_factor, 2.0));
+  }
+
+  DVLOG(1) << "Effective scaling-factor to be applied for Endless: " << cached_result;
+  return cached_result;
+}
+
 void GtkUi::ResetStyle() {
   LoadGtkValues();
   native_theme_->NotifyObservers();
@@ -1127,9 +1167,13 @@ float GtkUi::GetRawDeviceScaleFactor() {
       resolution <= 0 ? scale : resolution * scale / kDefaultDPI;
 #endif
 
+  // Endless can define an additional scaling factor to consider when on Composite
+  // mode that the browser will apply on top of whatever the current DPI value is.
+  float endless_factor = GetEndlessScalingFactor();
+
   // Blacklist scaling factors <120% (crbug.com/484400) and round
   // to 1 decimal to prevent rendering problems (crbug.com/485183).
-  return scale_factor < 1.2f ? 1.0f : roundf(scale_factor * 10) / 10;
+  return scale_factor < 1.2f ? 1.0f : roundf(endless_factor * scale_factor * 10) / 10;
 }
 
 void GtkUi::UpdateDeviceScaleFactor() {
