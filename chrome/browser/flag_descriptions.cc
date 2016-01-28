c7b6fbb12f00cd3e4655b76935a560cb1170db92
Mario Sanchez Prada <mario@endlessm.com>
2016-01-28 13:53:08 +0000

    Enable the tab discards feature
    This allows manually discarding tabs from chrome://discards as well
    as automatic tab discards once a certain level of "memory pressure"
    is reached, using Endless's implementation of MemoryPressureMonitor.
    
    [endlessm/eos-shell#1331]

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -3704,7 +3704,7 @@ const char kEnableInputImeApiDescription[] =
 
 #endif  // defined(OS_WIN) || defined(OS_LINUX)
 
-#if defined(OS_WIN) || defined(OS_MACOSX)
+#if defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_LINUX)
 
 const char kAutomaticTabDiscardingName[] = "Automatic tab discarding";
 const char kAutomaticTabDiscardingDescription[] =
@@ -3713,7 +3713,7 @@ const char kAutomaticTabDiscardingDescription[] =
     "reloaded when clicked on. Info about discarded tabs can be found at "
     "chrome://discards.";
 
-#endif  // defined(OS_WIN) || defined(OS_MACOSX)
+#endif  // defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_LINUX)
 
 #if defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_LINUX)
 
