02c28375dd79d6fa9999f56a14452b9083721dac
Mario Sanchez Prada <mario@endlessm.com>
2016-02-09 18:13:35 +0000

    Add an implementation of a MemoryPressureMonitor for Endless
    Based on the implementation for ChromeOS, with some tweaks:
    
    - Don't use filesystem-based notifications (/dev/chromeos-low-mem).
    - Don't record UMA histogram statistics (we don't need metrics).
    - Don't use different modes (e.g. agreesive/normal), just one.
    - Refactor the code a bit.
    
    https://phabricator.endlessm.com/T1331

--- content/browser/browser_main_loop.cc
+++ content/browser/browser_main_loop.cc
@@ -188,6 +188,10 @@
 #include "chromeos/chromeos_switches.h"
 #endif
 
+#if defined(OS_LINUX)
+#include "base/memory/memory_pressure_monitor_endless.h"
+#endif
+
 #if defined(USE_GLIB)
 #include <glib-object.h>
 #endif
@@ -1430,6 +1434,9 @@ void BrowserMainLoop::InitializeMemoryManagementComponent() {
 #elif defined(OS_WIN)
   memory_pressure_monitor_ =
       CreateWinMemoryPressureMonitor(parsed_command_line_);
+#elif defined(OS_LINUX)
+  memory_pressure_monitor_ =
+    std::make_unique<base::endless::MemoryPressureMonitor>();
 #endif
 
   if (base::FeatureList::IsEnabled(features::kMemoryCoordinator))
