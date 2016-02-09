9e1344b1468afe21b5a291c13f97f0ae850c37c7
Mario Sanchez Prada <mario@endlessm.com>
2016-02-09 18:13:35 +0000

    memory: Add an implementation of a MemoryPressureMonitor for Endless
    Based on the implementation for ChromeOS, with some tweaks:
    
    - Don't use filesystem-based notifications (/dev/chromeos-low-mem).
    - Don't record UMA histogram statistics (we don't need metrics).
    - Don't use different modes (e.g. agreesive/normal), just one.
    - Refactor the code a bit.
    
    Now that we include 25% of the zram in the available/total calculations, the
    tab discard threshold is set too high because if you have other apps loaded, we
    can easily leave the machine swapping/thrashing before we discard any tabs and
    relieve memory pressure. The "aggressive tab discard" level in ChromeOS is 70%
    so we adopt this value instead of our previous 80%.
    
    Conversely, we slightly raise the cache discard threshold because it's OK for
    those to be in memory and swapped to zram to some extent if other apps are
    being used, within reason. Our previous value was 45%, Chrome OS's "aggressive
    cache discard" level is 35% and the default is 60%. Because Endless OS has more
    overhead than Chrome OS, and other apps are more likely to be loaded, try 50%
    and see if we need to reduce it if low-memory machines feel a bit slow.
    
    https://phabricator.endlessm.com/T1331
    https://phabricator.endlessm.com/T23137

---
+++ base/memory/memory_pressure_monitor_endless.h
@@ -0,0 +1,84 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_MEMORY_MEMORY_PRESSURE_MONITOR_ENDLESS_H_
+#define BASE_MEMORY_MEMORY_PRESSURE_MONITOR_ENDLESS_H_
+
+#include "base/base_export.h"
+#include "base/macros.h"
+#include "base/memory/memory_pressure_listener.h"
+#include "base/memory/memory_pressure_monitor.h"
+#include "base/memory/weak_ptr.h"
+#include "base/timer/timer.h"
+
+namespace base {
+
+namespace endless {
+
+class BASE_EXPORT MemoryPressureMonitor : public base::MemoryPressureMonitor {
+ public:
+  explicit MemoryPressureMonitor();
+  ~MemoryPressureMonitor() override;
+
+  void Start() override;
+
+  // Get the current memory pressure level.
+  MemoryPressureListener::MemoryPressureLevel GetCurrentPressureLevel() const override;
+
+  // Sets a notification callback. The default callback invokes
+  // base::MemoryPressureListener::NotifyMemoryPressure.
+  void SetDispatchCallback(const DispatchCallback& callback) override;
+
+  // Returns a type-casted version of the current memory pressure monitor. A
+  // simple wrapper to base::MemoryPressureMonitor::Get.
+  static MemoryPressureMonitor* Get();
+
+ private:
+  // Starts observing the memory fill level.
+  // Calls to StartObserving should always be matched with calls to
+  // StopObserving.
+  void StartObserving();
+
+  // Stop observing the memory fill level.
+  // May be safely called if StartObserving has not been called.
+  void StopObserving();
+
+  // The function which gets periodically called to check any changes in the
+  // memory pressure. It will report pressure changes as well as continuous
+  // critical pressure levels.
+  void CheckMemoryPressure();
+
+  // The function periodically checks the memory pressure changes and records
+  // the UMA histogram statistics for the current memory pressure level.
+  void CheckMemoryPressureAndRecordStatistics();
+
+  // Returns the correct MemoryPressureLevel according to the moderate and critical tresholds.
+  MemoryPressureListener::MemoryPressureLevel GetMemoryPressureLevelFromPercent(int percent);
+
+  // Get the memory pressure in percent.
+  int GetUsedMemoryInPercent();
+
+  // The current memory pressure.
+  base::MemoryPressureListener::MemoryPressureLevel current_memory_pressure_level_;
+
+  // A periodic timer to check for resource pressure changes. This will get
+  // replaced by a kernel triggered event system (see crbug.com/381196).
+  base::RepeatingTimer timer_;
+
+  // To slow down the amount of moderate pressure event calls, this counter
+  // gets used to count the number of events since the last event occured.
+  int moderate_pressure_repeat_count_;
+
+  DispatchCallback dispatch_callback_;
+
+  base::WeakPtrFactory<MemoryPressureMonitor> weak_ptr_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(MemoryPressureMonitor);
+};
+
+} // namespace endless
+
+}  // namespace base
+
+#endif  // BASE_MEMORY_MEMORY_PRESSURE_MONITOR_ENDLESS_H_
