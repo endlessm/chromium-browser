291ec5e5df84c17731192da49f1a41c11a818b0b
Mario Sanchez Prada <mario@endlessm.com>
2016-02-09 18:05:20 +0000

    memory: Add cmdline switches to override memory pressure monitor's default thesholds
    New parameters:
    
      --memory-pressure-moderate-threshold: integer in [0, 100)
      --memory-pressure-critical-threshold: integer in (MODERATE, 100)
    
    https://phabricator.endlessm.com/T1331

--- base/base_switches.cc
+++ base/base_switches.cc
@@ -146,4 +146,10 @@ const char kEnableReachedCodeProfiler[] = "enable-reached-code-profiler";
 const char kEnableThreadInstructionCount[] = "enable-thread-instruction-count";
 #endif
 
+// Used to override the default thresholds used by the MemoryPressureMonitor
+// to notify the MemoryPressureListener that a relevant event has occurred.
+// If defined they must be integers between [0, 100], with CRITICAL > MODERATE.
+const char kMemoryPressureModerateThreshold[] = "memory-pressure-moderate-threshold";
+const char kMemoryPressureCriticalThreshold[] = "memory-pressure-critical-threshold";
+
 }  // namespace switches
