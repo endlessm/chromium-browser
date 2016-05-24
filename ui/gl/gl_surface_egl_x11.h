03bd501d74ac81b08a3bc24fcec86e157c8b4b4a
Mario Sanchez Prada <mario@endlessm.com>
2016-05-24 14:44:41 +0100

    egl/x11: Don't use child XWindow to control resizes, it crashes our Mali driver
    This change has been added upstream to help controlling resizes of EGL surfaces
    without exposing bugs in some drivers in a similar way as it's done for GLX,
    but it has the nasty side effect of causing crashes with our Mali driver, so
    we need to partially revert the patch from bug 326995 to avoid that downstream.
    
    To do that without minimal changes to the code, just remove all the stuff from
    the X11 implementation that deals with creating a child XWindow and handling
    events, and simply leave the override of GetConfig() that will be used from
    the parent class, GLSurfaceEGL.
    
    Relevant commit: https://chromium.googlesource.com/chromium/src/+/93f34d7a
    
    merge with egl/x11: Don't use child XWindow to control resizes

--- ui/gl/gl_surface_egl_x11.h
+++ ui/gl/gl_surface_egl_x11.h
@@ -10,34 +10,20 @@
 #include <string>
 
 #include "base/macros.h"
-#include "ui/events/platform/platform_event_dispatcher.h"
-#include "ui/gl/gl_export.h"
 #include "ui/gl/gl_surface_egl.h"
 
 namespace gl {
 
 // Encapsulates an EGL surface bound to a view using the X Window System.
-class GL_EXPORT NativeViewGLSurfaceEGLX11 : public NativeViewGLSurfaceEGL,
-                                            public ui::PlatformEventDispatcher {
+class GL_EXPORT NativeViewGLSurfaceEGLX11 : public NativeViewGLSurfaceEGL {
  public:
   explicit NativeViewGLSurfaceEGLX11(EGLNativeWindowType window);
 
   // NativeViewGLSurfaceEGL overrides.
   EGLConfig GetConfig() override;
-  void Destroy() override;
-  bool Resize(const gfx::Size& size,
-              float scale_factor,
-              bool has_alpha) override;
-  bool InitializeNativeWindow() override;
 
  private:
   ~NativeViewGLSurfaceEGLX11() override;
-
-  EGLNativeWindowType parent_window_;
-
-  // PlatformEventDispatcher implementation.
-  bool CanDispatchEvent(const ui::PlatformEvent& event) override;
-  uint32_t DispatchEvent(const ui::PlatformEvent& event) override;
 
   DISALLOW_COPY_AND_ASSIGN(NativeViewGLSurfaceEGLX11);
 };
