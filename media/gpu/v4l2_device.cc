39e4421a6fda4be73839f94a44674dd7179214b4
Michael Catanzaro <mcatanzaro@igalia.com>
2016-08-01 13:15:51 -0500

    v4l2: Workaround build issues on ARM with old V4L2 driver
    The V4L2 driver we currently ship on Endless does not define slice
    based codec profiles (e.g. V4L2_PIX_FMT_H264_SLICE), nor implement
    support for hardware decoding of the VP9 codec, meaning that we
    need to comment out a bunch of sections in the code to compile.
    
    Fortunately, this sections of code were not used in any case in
    endless so commenting them out does not cause regressions.
    
    This commit should be removed once our V4L2 driver includes
    support for all the defines ignored here.
    
    https://phabricator.endlessm.com/T12671
    https://phabricator.endlessm.com/T14005

--- media/gpu/v4l2_device.cc
+++ media/gpu/v4l2_device.cc
@@ -53,8 +53,10 @@ VideoPixelFormat V4L2Device::V4L2PixFmtToVideoPixelFormat(uint32_t pix_fmt) {
     case V4L2_PIX_FMT_YVU420:
       return PIXEL_FORMAT_YV12;
 
+#if 0
     case V4L2_PIX_FMT_YUV422M:
       return PIXEL_FORMAT_I422;
+#endif
 
     case V4L2_PIX_FMT_RGB32:
       return PIXEL_FORMAT_ARGB;
@@ -89,21 +91,35 @@ uint32_t V4L2Device::VideoPixelFormatToV4L2PixFmt(VideoPixelFormat format) {
 // static
 uint32_t V4L2Device::VideoCodecProfileToV4L2PixFmt(VideoCodecProfile profile,
                                                    bool slice_based) {
+  if (slice_based){
+    LOG(FATAL) << "Endless does not support the V4L2 slice video decode accelerator!";
+    return 0;
+  }
+
   if (profile >= H264PROFILE_MIN && profile <= H264PROFILE_MAX) {
+#if 0
     if (slice_based)
       return V4L2_PIX_FMT_H264_SLICE;
     else
+#endif
       return V4L2_PIX_FMT_H264;
   } else if (profile >= VP8PROFILE_MIN && profile <= VP8PROFILE_MAX) {
+#if 0
     if (slice_based)
       return V4L2_PIX_FMT_VP8_FRAME;
     else
+#endif
       return V4L2_PIX_FMT_VP8;
   } else if (profile >= VP9PROFILE_MIN && profile <= VP9PROFILE_MAX) {
+#if 0
     if (slice_based)
       return V4L2_PIX_FMT_VP9_FRAME;
     else
       return V4L2_PIX_FMT_VP9;
+#endif
+
+    LOG(FATAL) << "Endless does not support the V4L2 VP9 accelerator yet";
+    return 0;
   } else {
     LOG(FATAL) << "Add more cases as needed";
     return 0;
@@ -119,7 +135,9 @@ std::vector<VideoCodecProfile> V4L2Device::V4L2PixFmtToVideoCodecProfiles(
 
   switch (pix_fmt) {
     case V4L2_PIX_FMT_H264:
+#if 0
     case V4L2_PIX_FMT_H264_SLICE:
+#endif
       if (is_encoder) {
         // TODO(posciak): need to query the device for supported H.264 profiles,
         // for now choose Main as a sensible default.
@@ -132,16 +150,20 @@ std::vector<VideoCodecProfile> V4L2Device::V4L2PixFmtToVideoCodecProfiles(
       break;
 
     case V4L2_PIX_FMT_VP8:
+#if 0
     case V4L2_PIX_FMT_VP8_FRAME:
+#endif
       min_profile = VP8PROFILE_MIN;
       max_profile = VP8PROFILE_MAX;
       break;
 
+#if 0
     case V4L2_PIX_FMT_VP9:
     case V4L2_PIX_FMT_VP9_FRAME:
       min_profile = VP9PROFILE_MIN;
       max_profile = VP9PROFILE_MAX;
       break;
+#endif
 
     default:
       DVLOG(1) << "Unhandled pixelformat " << std::hex << "0x" << pix_fmt;
@@ -170,9 +192,10 @@ uint32_t V4L2Device::V4L2PixFmtToDrmFormat(uint32_t format) {
 
     case V4L2_PIX_FMT_RGB32:
       return DRM_FORMAT_ARGB8888;
-
+#if 0
     case V4L2_PIX_FMT_MT21:
       return DRM_FORMAT_MT21;
+#endif
 
     default:
       DVLOG(1) << "Unrecognized format " << std::hex << "0x" << format;
