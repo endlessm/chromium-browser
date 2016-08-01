3ab16b60dae5e518dd52cfaa034a6e1ba75ef1b8
Michael Catanzaro <mcatanzaro@igalia.com>
2016-08-01 13:15:51 -0500

    v4l2: Workaround build issues on ARM with old V4L2 driver
    The V4L2 driver we currently ship on Endless does not define slice
    based codec profiles (e.g. V4L2_PIX_FMT_H264_SLICE), meaning that
    we need to comment out a bunch of sections in the code to compile.
    
    Fortunately, this sections of code were not used in any case in
    endless so commenting them out does not cause regressions.
    
    This commit should be removed once our V4L2 driver includes
    support for all the defines ignored here.
    
    https://phabricator.endlessm.com/T12671
    https://phabricator.endlessm.com/T14005

--- media/gpu/v4l2/v4l2_device.cc
+++ media/gpu/v4l2/v4l2_device.cc
@@ -1088,8 +1088,10 @@ VideoPixelFormat V4L2Device::V4L2PixFmtToVideoPixelFormat(uint32_t pix_fmt) {
     case V4L2_PIX_FMT_YVU420M:
       return PIXEL_FORMAT_YV12;
 
+#if 0
     case V4L2_PIX_FMT_YUV422M:
       return PIXEL_FORMAT_I422;
+#endif
 
     case V4L2_PIX_FMT_RGB32:
       return PIXEL_FORMAT_ARGB;
@@ -1128,20 +1130,31 @@ uint32_t V4L2Device::VideoFrameLayoutToV4L2PixFmt(
 // static
 uint32_t V4L2Device::VideoCodecProfileToV4L2PixFmt(VideoCodecProfile profile,
                                                    bool slice_based) {
+  if (slice_based){
+    LOG(FATAL) << "Endless does not support the V4L2 slice video decode accelerator!";
+    return 0;
+  }
+
   if (profile >= H264PROFILE_MIN && profile <= H264PROFILE_MAX) {
+#if 0
     if (slice_based)
       return V4L2_PIX_FMT_H264_SLICE;
     else
+#endif
       return V4L2_PIX_FMT_H264;
   } else if (profile >= VP8PROFILE_MIN && profile <= VP8PROFILE_MAX) {
+#if 0
     if (slice_based)
       return V4L2_PIX_FMT_VP8_FRAME;
     else
+#endif
       return V4L2_PIX_FMT_VP8;
   } else if (profile >= VP9PROFILE_MIN && profile <= VP9PROFILE_MAX) {
+#if 0
     if (slice_based)
       return V4L2_PIX_FMT_VP9_FRAME;
     else
+#endif
       return V4L2_PIX_FMT_VP9;
   } else {
     LOG(ERROR) << "Unknown profile: " << GetProfileName(profile);
@@ -1176,7 +1189,9 @@ std::vector<VideoCodecProfile> V4L2Device::V4L2PixFmtToVideoCodecProfiles(
 
   switch (pix_fmt) {
     case V4L2_PIX_FMT_H264:
+#if 0
     case V4L2_PIX_FMT_H264_SLICE:
+#endif
       if (is_encoder) {
         // TODO(posciak): need to query the device for supported H.264 profiles,
         // for now choose Main as a sensible default.
@@ -1187,15 +1202,18 @@ std::vector<VideoCodecProfile> V4L2Device::V4L2PixFmtToVideoCodecProfiles(
         max_profile = H264PROFILE_MAX;
       }
       break;
-
     case V4L2_PIX_FMT_VP8:
+#if 0
     case V4L2_PIX_FMT_VP8_FRAME:
+#endif
       min_profile = VP8PROFILE_MIN;
       max_profile = VP8PROFILE_MAX;
       break;
 
-    case V4L2_PIX_FMT_VP9:
+    case V4L2_PIX_FMT_VP9: {
+#if 0
     case V4L2_PIX_FMT_VP9_FRAME: {
+#endif
       v4l2_queryctrl query_ctrl = {};
       query_ctrl.id = V4L2_CID_MPEG_VIDEO_VP9_PROFILE;
       if (Ioctl(VIDIOC_QUERYCTRL, &query_ctrl) == 0) {
