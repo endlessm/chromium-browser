d54d36080d17fa9c2dfe985ec9233aef41b1f4a3
Mario Sanchez Prada <mario@endlessm.com>
2016-08-12 14:12:40 +0000

    Enable MP3 on the non-extra version of chromium-codecs-ffmpeg
    Chromium bundles FFMPEG without adapting its original build system, which
    required some ingenious scripting machinery to generate the right GYP/GN
    files depending on the build options passed, which are also different
    depending on the branding used (Chrome, Chromium, ChromeOS or ChromiumOS)
    and the target architecture. So, this is not as simple as changing a flag.
    
    Instead, we need to do a few things to make sure things are in place:
    
      1. Adapt the build_ffmpeg.py, so that it configures FFMPEG as expected
         depending on the target OS and Architecture.
    
      2. Use build_ffmpeg.py to build FFMPEG for each target platform, so that
         we get the right defines in the config.{asm|h} files for each OS/Arch.
    
      3. Update the ffmpeg_generated.gypi file so that the right sources are
         selected depending on the build options of each version of FFMPEG.
    
    Unfortunately, generating the ffmpeg_generated.gypi file automatically requires
    building FFMPEG for each target platform first which, according to the "Chromium
    FFmpeg Roll Instructions" document, requires the developer to setup a ChromiumOS
    development environment first, which seems a bit overkill for what we want
    to do here (just enable MP3 for the "Chromium" branding).
    
    So, the solution implemented here was a combination of using some scripts together
    with some manual work, so that we don't need a ChromiumOS environment at all.
    
    Therefore, the following steps were followed to create this patch:
    
      1. First of all, I used an Ubuntu Trusty chroot as the base Linux system.
      2. Modify chromium/scripts/build_ffmpeg.py to include what we want in each OS/ARCH combination of FFMPEG
      3. Build FFMPEG for ia32 (steps 1 only needed the first time):
         1. Install yasm:
             $ sudo apt-get install yasm
         2. Build FFMPEG for the target architecture now:
             $ chromium/scripts/build_ffmpeg.py linux ia32
      4. Update the local configuration:
          $ chromium/scripts/copy_config.sh
      5. Manual work now:
         1) Initially stage the changes to the config files for ia32 and remove everything else (build.ia32.linux dir too)
         2) Manually replicate the changes applied to the chromium/config/Chromium/linux/ia32/config.{asm|h} files in all
            the interesting (for us) OS/ARCH combinations: linux|linux-noasm / ia32|x64|arm|arm-neon|arm64|mipsel
         3) Carefully adapt ffmpeg_generated.gypi to include the right source files in every OS/Arch combination.
    
    Note that, to make future rebases potentially easier, all the changes added into the
    ffmpeg_generated.gypi file were appended at the end instead of trying to re-organize
    the already present rules, hoping that it will help deal better with possible conflicts
    should newer versions of this bundled FFMPEG change substantially compare to this one.
    
    Also, we only manually modified the generated file for GYP, but not the one for GN, as
    we are not using that build system at the moment. But that might very well change in the
    future, so we need to be aware of that too.
    
    References:
    ----------
    Chromium FFmpeg Roll Instructions:
    https://docs.google.com/document/d/14bqZ9NISsyEO3948wehhJ7wc9deTIz-yHUhF1MQp7Po
    
    Chromium OS Developer Guide:
    http://www.chromium.org/chromium-os/developer-guide
    
    https://phabricator.endlessm.com/T12752

--- third_party/ffmpeg/chromium/scripts/build_ffmpeg.py
+++ third_party/ffmpeg/chromium/scripts/build_ffmpeg.py
@@ -530,6 +530,15 @@ def main(argv):
           '--ar=cygwin-wrapper lib',
       ])
 
+  # Additional common configuration for Endless OS.
+  if target_os in ['linux', 'linux-noasm']:
+    configure_flags['ChromiumEndless'].extend([
+        # Enable MP3 by default.
+        '--enable-decoder=mp3',
+        '--enable-demuxer=mp3',
+        '--enable-parser=mpegaudio',
+    ])
+
   # Google Chrome & ChromeOS specific configuration.
   configure_flags['Chrome'].extend([
       '--enable-decoder=aac,h264,mp3',
@@ -589,6 +598,7 @@ def main(argv):
     do_build_ffmpeg('Chromium',
                     configure_flags['Common'] +
                     configure_flags['Chromium'] +
+                    configure_flags['ChromiumEndless'] +
                     configure_flags['EnableLTO'] +
                     configure_args)
     do_build_ffmpeg('Chrome',
