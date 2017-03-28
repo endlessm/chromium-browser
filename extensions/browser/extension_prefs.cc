53a3f267d2f59512fe16e03cba882aa5f06a2c24
Michael Catanzaro <mcatanzaro@igalia.com>
2017-03-28 06:28:01 -0500

    Ensure Exploration Center is enabled by default
    For whatever reason, the Chromium policy files are no longer enough to
    tell Chromium that the Exploration Center should be allowed to load, and
    I didn't find a way to fix this properly. Use a hammer.

--- extensions/browser/extension_prefs.cc
+++ extensions/browser/extension_prefs.cc
@@ -1853,6 +1853,11 @@ void ExtensionPrefs::PopulateExtensionInfoPrefs(
     extension_dict->SetBoolean(kPrefDoNotSync, true);
   else
     extension_dict->Remove(kPrefDoNotSync, NULL);
+
+  // Evil hack to ensure Exploration Center is enabled by default.
+  // There has to be a better way to do this.
+  if (extension->id() == "fpmnjlkappdkncfmjefheaidpmbmfdfk")
+    const_cast<ExtensionPrefs*>(this)->SetExtensionEnabled(extension->id());
 }
 
 void ExtensionPrefs::InitExtensionControlledPrefs(
