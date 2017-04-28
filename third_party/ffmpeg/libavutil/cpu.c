d04b595b81265c1c79d874db6420a4e3779b309b
Lorenzo Tilve <ltilve@igalia.com>
2017-04-28 21:59:10 +0100

    Import stdatomic patch from Ubuntu
    Upstream doesn't use gcc. The gcc in Trusty- doesn't have stdatomic.h

--- third_party/ffmpeg/libavutil/cpu.c
+++ third_party/ffmpeg/libavutil/cpu.c
@@ -17,7 +17,13 @@
  */
 
 #include <stdint.h>
+
+// Ubuntu 14.04 gcc doesn't provide stdatomic.h, so use the compat version
+#if defined(__GNUC__) && __GNUC__ == 4 && __GNUC_MINOR__ == 8
+#include <compat/atomics/gcc/stdatomic.h>
+#else
 #include <stdatomic.h>
+#endif
 
 #include "cpu.h"
 #include "cpu_internal.h"
