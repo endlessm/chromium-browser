5979cf427c5f6d1462751348575d0fee83fec922
Carlo Caione <carlo@endlessm.com>
2017-05-16 12:05:02 +0200

    MimeUtil: advertise WebM support only on AArch64
    Leave WebM support disabled only on armv7l (S805 / Endless Mini) but enable it
    for S905X (AArch64).
    
    To be considered as a HACK until a better solution is found.
    
    Signed-off-by: Carlo Caione <carlo@endlessm.com>

--- media/base/mime_util_internal.cc
+++ media/base/mime_util_internal.cc
@@ -10,6 +10,7 @@
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
+#include "base/system/sys_info.h"
 #include "build/build_config.h"
 #include "media/base/media.h"
 #include "media/base/media_client.h"
@@ -979,7 +980,10 @@ SupportsType MimeUtil::IsCodecSupported(const std::string& mime_type_lower_case,
 #endif
 
 #ifdef __arm__
-  if (codec == MimeUtil::VP8 || codec == MimeUtil::VP9)
+  // XXX: HACK
+  std::string arch = base::SysInfo::OperatingSystemArchitecture();
+
+  if (arch == "armv7l" && (codec == MimeUtil::VP8 || codec == MimeUtil::VP9))
     return IsNotSupported;
 #endif
 
