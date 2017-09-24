961dcf39a91a650a13eb0a65b96f29484b87d21d
Lorenzo Tilve <ltilve@igalia.com>
2017-09-24 20:02:48 +0200

    Import relax-ninja-version-requirement.patch from Ubuntu

--- tools/gn/ninja_build_writer.cc
+++ tools/gn/ninja_build_writer.cc
@@ -242,7 +242,7 @@ bool NinjaBuildWriter::RunAndWriteFile(
 }
 
 void NinjaBuildWriter::WriteNinjaRules() {
-  out_ << "ninja_required_version = 1.7.2\n\n";
+  out_ << "ninja_required_version = 1.5.1\n\n";
   out_ << "rule gn\n";
   out_ << "  command = " << GetSelfInvocationCommand(build_settings_) << "\n";
   out_ << "  description = Regenerating ninja files\n\n";
