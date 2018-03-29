3d0b51bfecd20869d04df07b0a61d7a7cdfda48b
Lorenzo Tilve <ltilve@igalia.com>
2018-03-29 19:55:19 +0200

    Import enable-chromecast-by-default.patch from Ubuntu

--- extensions/common/feature_switch.cc
+++ extensions/common/feature_switch.cc
@@ -46,11 +46,7 @@ class CommonSwitches {
                          FeatureSwitch::DEFAULT_ENABLED),
         load_media_router_component_extension(
             kLoadMediaRouterComponentExtensionFlag,
-#if defined(GOOGLE_CHROME_BUILD)
             FeatureSwitch::DEFAULT_ENABLED)
-#else
-            FeatureSwitch::DEFAULT_DISABLED)
-#endif  // defined(GOOGLE_CHROME_BUILD)
   {
   }
 
