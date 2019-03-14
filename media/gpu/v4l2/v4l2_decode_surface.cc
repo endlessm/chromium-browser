aaf14e9146e101a338ec62c7b21045820dbacf1c
Lorenzo Tilve <ltilve@igalia.com>
2019-03-14 11:53:38 +0100

    v4l2: avoid abstraction of config store management in V4L2DecodeSurface
    The usage of config_store was abstracted on this commit:
    https://chromium-review.googlesource.com/c/chromium/src/+/1388067
    
    Which was breaking the build on the meson kernel as it is not supported
    on our meson version.
    
    This commits also partially reverts other subsequent
    changes as the following ones that were relying on that unsupported
    abstraction.
    
    Revert "media/gpu: implement V4L2SliceVideoDecoder"
    https://chromium-review.googlesource.com/c/chromium/src/+/1519907
    
    Revert "media/gpu/v4l2: initial support for request API"
    https://chromium-review.googlesource.com/c/chromium/src/+/1388068

--- media/gpu/v4l2/v4l2_decode_surface.cc
+++ media/gpu/v4l2/v4l2_decode_surface.cc
@@ -5,7 +5,6 @@
 #include "media/gpu/v4l2/v4l2_decode_surface.h"
 
 #include <linux/media.h>
-#include <linux/videodev2.h>
 #include <poll.h>
 #include <sys/ioctl.h>
 
@@ -94,74 +93,6 @@ std::string V4L2DecodeSurface::ToString() const {
     base::StringAppendF(&out, " %d", ref->output_record());
   }
   return out;
-}
-
-void V4L2ConfigStoreDecodeSurface::PrepareSetCtrls(
-    struct v4l2_ext_controls* ctrls) const {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  DCHECK_NE(ctrls, nullptr);
-  DCHECK_GT(config_store_, 0u);
-
-  ctrls->config_store = config_store_;
-}
-
-void V4L2ConfigStoreDecodeSurface::PrepareQueueBuffer(
-    struct v4l2_buffer* buffer) const {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  DCHECK_NE(buffer, nullptr);
-  DCHECK_GT(config_store_, 0u);
-
-  buffer->config_store = config_store_;
-}
-
-uint64_t V4L2ConfigStoreDecodeSurface::GetReferenceID() const {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-
-  // Control store uses the output buffer ID as reference.
-  return output_record();
-}
-
-bool V4L2ConfigStoreDecodeSurface::Submit() {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-
-  // There is nothing extra to submit when using the config store
-  return true;
-}
-
-void V4L2RequestDecodeSurface::PrepareSetCtrls(
-    struct v4l2_ext_controls* ctrls) const {
-  DCHECK_NE(ctrls, nullptr);
-  DCHECK(request_ref_.IsValid());
-
-  ctrls->which = V4L2_CTRL_WHICH_REQUEST_VAL;
-  request_ref_.SetCtrls(ctrls);
-}
-
-void V4L2RequestDecodeSurface::PrepareQueueBuffer(
-    struct v4l2_buffer* buffer) const {
-  DCHECK_NE(buffer, nullptr);
-  DCHECK(request_ref_.IsValid());
-
-  request_ref_.SetQueueBuffer(buffer);
-  // Use the output buffer index as the timestamp.
-  // Since the client is supposed to keep the output buffer out of the V4L2
-  // queue for as long as it is used as a reference frame, this ensures that
-  // all the requests we submit have unique IDs at any point in time.
-  DCHECK_EQ(static_cast<int>(buffer->index), input_record());
-  buffer->timestamp.tv_sec = 0;
-  buffer->timestamp.tv_usec = output_record();
-}
-
-uint64_t V4L2RequestDecodeSurface::GetReferenceID() const {
-  // Convert the input buffer ID to what the internal representation of
-  // the timestamp we submitted will be (tv_usec * 1000).
-  return output_record() * 1000;
-}
-
-bool V4L2RequestDecodeSurface::Submit() {
-  DCHECK(request_ref_.IsValid());
-
-  return std::move(request_ref_).Submit().IsValid();
 }
 
 }  // namespace media
