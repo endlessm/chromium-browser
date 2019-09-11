3b2adbb8b27cb1269d95fbef9c2fb7b67bd58a85
Lorenzo Tilve <ltilve@igalia.com>
2019-09-11 11:20:58 +0200

    Revert "media/gpu/v4l2: initial support for request API"
    This reverts commit 16e00de47301941e1a77bb88494fe6ef8206b39e.

--- media/gpu/v4l2/v4l2_slice_video_decode_accelerator.h
+++ media/gpu/v4l2/v4l2_slice_video_decode_accelerator.h
@@ -14,7 +14,6 @@
 #include <vector>
 
 #include "base/containers/queue.h"
-#include "base/files/scoped_file.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
@@ -85,8 +84,6 @@ class MEDIA_GPU_EXPORT V4L2SliceVideoDecodeAccelerator
     size_t length;
     size_t bytes_used;
     bool at_device;
-    // Request fd used for this input buffer if request API is used.
-    base::ScopedFD request_fd;
   };
 
   // Record for output buffers.
@@ -391,10 +388,6 @@ class MEDIA_GPU_EXPORT V4L2SliceVideoDecodeAccelerator
   std::list<int> free_input_buffers_;
   // Mapping of int index to an input buffer record.
   std::vector<InputRecord> input_buffer_map_;
-  // Set to true by CreateInputBuffers() if the codec driver supports requests
-  bool supports_requests_ = false;
-  // Stores the media file descriptor if request API is used
-  base::ScopedFD media_fd_;
 
   // Output queue state.
   bool output_streamon_;
