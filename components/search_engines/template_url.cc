a4b0c423cadfefa26f2f0efcdbe39ef4dcf546be
Andre Moreira Magalhaes <andre@endlessm.com>
2020-05-25 13:10:26 -0300

    Revert "Import search-credit.patch from Ubuntu"
    This reverts commit 56e4fd22e626f632325b7b7762d53506ace1b238.

--- components/search_engines/template_url.cc
+++ components/search_engines/template_url.cc
@@ -958,9 +958,6 @@ std::string TemplateURLRef::HandleReplacements(
 
   std::string url = parsed_url_;
 
-  bool is_google_search = false;
-  bool ubuntu_credit_for_search = false;
-
   // replacements_ is ordered in ascending order, as such we need to iterate
   // from the back.
   for (auto i = replacements_.rbegin(); i != replacements_.rend(); ++i) {
@@ -1031,7 +1028,6 @@ std::string TemplateURLRef::HandleReplacements(
 
       case GOOGLE_BASE_URL:
         DCHECK(!i->is_post_param);
-        is_google_search = true;
         HandleReplacement(
             std::string(), search_terms_data.GoogleBaseURLValue(), *i, &url);
         break;
@@ -1126,8 +1122,7 @@ std::string TemplateURLRef::HandleReplacements(
 
       case GOOGLE_SEARCH_CLIENT: {
         DCHECK(!i->is_post_param);
-        ubuntu_credit_for_search = true;
-        std::string client = "ubuntu";
+        std::string client = search_terms_data.GetSearchClient();
         if (!client.empty())
           HandleReplacement("client", client, *i, &url);
         break;
@@ -1253,13 +1248,6 @@ std::string TemplateURLRef::HandleReplacements(
       default:
         NOTREACHED();
         break;
-    }
-  }
-
-  /* Google search template from prefs might not have a client token. */
-  if (is_google_search && !ubuntu_credit_for_search) {
-    if (url.find_first_of('?', 8) != std::string::npos) {  // perhaps no GET params
-      url.append("&client=ubuntu");
     }
   }
 
