591bac11f848332733e3b718be945bc9733b1b33
Andre Moreira Magalhaes <andre@endlessm.com>
2020-05-25 13:07:15 -0300

    Import node-use-system-wide.patch from Ubuntu

--- third_party/node/node.py
+++ third_party/node/node.py
@@ -3,18 +3,11 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from os import path as os_path
-import platform
 import subprocess
-import sys
 
 
 def GetBinaryPath():
-  return os_path.join(os_path.dirname(__file__), *{
-    'Darwin': ('mac', 'node-darwin-x64', 'bin', 'node'),
-    'Linux': ('linux', 'node-linux-x64', 'bin', 'node'),
-    'Windows': ('win', 'node.exe'),
-  }[platform.system()])
+  return '/usr/bin/node'
 
 
 def RunNode(cmd_parts, stdout=None):
